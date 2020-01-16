dff00fc973bbe1ac70dbd602560d43260220bc70
Landon Morgan <morganlandon@gmail.com>
2020-01-16 11:26:17 -0600

    added main.js file, JavaScript code to update 'article__header' class

---
+++ main.js
@@ -0,0 +1,5 @@
+/* Use JavaScript to obtain a reference to the first section with the
+class of article_header and change its text with the textContent property
+to "Welcome to the {insert your name here} blog" */
+
+document.querySelector(".article__header").textContent = "Welcome to the Landon Morgan blog";
